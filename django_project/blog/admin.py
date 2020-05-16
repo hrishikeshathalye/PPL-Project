64a367462b69745fda7c67e34da36e015ba10903
Viren <viren.p2000@gmail.com>
2020-05-16 19:19:47 +0530

    Added back navigation and its icon.

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -4,3 +4,6 @@ from .models import Post, Friend, Comment
 admin.site.register(Post)
 admin.site.register(Friend)
 admin.site.register(Comment)
+admin.site.site_header = 'Covid Connect Admin'
+admin.site.site_title = 'Covid Connect Admin'
+admin.site.index_title = 'Welcome to the Covid Connect Admin Area!'
